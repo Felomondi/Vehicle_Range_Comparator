8f64f698559bcbb2f7ad25e6ee8f674241030e45
Felix Omondi <114206850+Felomondi@users.noreply.github.com>
2023-03-29 14:33:05 -0400

    Add files via upload

---
+++ AVehicle.java
@@ -0,0 +1,20 @@
+
+/**
+ *
+ * @author (Felix)
+ */
+public abstract class AVehicle implements IVehicle {
+    private int year;
+    private String name;
+
+    protected AVehicle(int year, String name) {
+        this.year = year;
+        this.name = name;
+    }
+
+    public String toString() {
+        return year + " " + name;
+    }
+
+    public abstract double range();
+}
